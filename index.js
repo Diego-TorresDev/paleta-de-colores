1a938d79f6f538261c653d736bbd3f83b82327c4
Diego Torres <andres354859@gmail.com>
2023-04-27 13:36:13 -0500

    Instalando express y ejecutando mi primer servidor

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
-const mysql      = require('mysql');
+const express = require('express');
+const app = express();
+const port  = 3000;
 
-const connection = mysql.createConnection({
-
-  host     : 'pintuco.ca4mymnxbdq2.us-east-1.rds.amazonaws.com',
-
-  user     : 'admin',
-
-  password : 'P1ntuc0B4s3d3d4t0s',
-
-  database : 'abanicodecolores.pintuco.com2'
-
+app.get('/', (req, res) => {
+    res.send('hola');
 });
 
- 
+app.listen(port, () => {
+    console.log(port);
+})
 
-connection.connect();
-
-
-
-
-console.log("Comienza")
-
-connection.query(`SELECT * FROM upload_excel_colombia uec WHERE gama = 'Rojos' GROUP BY codigo_color ; `, function (error, results, fields) {
-
-    if (error) throw error;
-
-    console.log('The solution is: ', results);
-
-  })
-
-
-
-
-connection.end();
