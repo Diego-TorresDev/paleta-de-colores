7a20a659ea9ce43754a725f7ceb2db850b790b99
Diego Torres <andres354859@gmail.com>
2023-04-27 14:52:13 -0500

    se genereo una consulta con mysql y se muestra mediante una ruta

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port  = 3000;
 
-app.get('/', (req, res) => {
-    res.send('hola');
+const connection = mysql.createConnection({
+host : 'pintuco.ca4mymnxbdq2.us-east-1.rds.amazonaws.com',
+user : 'admin',
+password : 'P1ntuc0B4s3d3d4t0s',
+database : 'abanicodecolores.pintuco.com2'
 });
+
+let results;
+connection.connect();
+console.log("Comienza")
+    connection.query(`SELECT * FROM upload_excel_colombia uec WHERE gama = 'Rojos' and producto = 'Viniltex Advanced' GROUP BY codigo_color ;`, function (error, results, fields) {
+    
+    if (error) throw error;
+        console.log('The solution is: ', results);
+        app.get('/', (req, res) => {
+            res.send(results);
+        });
+    });
+
+
+
+
+
+app.get('/api/:color/:superficie', (req, res) => {
+    const { color } = req.query;
+    const { superficie } = req.query;
+
+    res.json({
+        color,
+        superficie
+    });
+});
+
 
 app.listen(port, () => {
     console.log(port);
